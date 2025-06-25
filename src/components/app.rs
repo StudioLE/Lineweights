d961dae96bc6938f3a418e529c4be838f24972e5
Laurence Elsdon <git@elsdon.me>
2025-06-25 17:12:30 +0100

    chore: Move `App` to individual file. Remove verbose comments

---
+++ src/components/app.rs
@@ -0,0 +1,22 @@
+use super::*;
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+/// App is the main component of our app. Components are the building blocks of dioxus apps.
+/// Each component is a function that takes some props and returns an Element. In this case,
+/// App takes no props because it is the root of our app.
+///
+/// Components should be annotated with `#[component]` to support props, better error messages,
+/// and autocomplete
+#[component]
+pub(crate) fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        Hero {}
+    }
+}
