1a64f4d7afb852c5140b2ef57934ffd8fa4e380e
Laurence Elsdon <git@elsdon.me>
2025-06-27 21:00:14 +0100

    feat: Per dose colors

--- src/components/chart.rs
+++ src/components/chart.rs
@@ -14,9 +14,10 @@ pub fn Chart() -> Element {
     let total_days = (max_date - min_date).num_days();
     let (min_weight, max_weight) = get_weight_range(&entries)?;
     let weight_span = max_weight - min_weight;
+    let view_box = format!("-1 {} {} {}", min_weight.floor() - 1.0, total_days + 2, weight_span.ceil() + 2.0);
     rsx! {
         svg {
-            view_box: "0 {min_weight} {total_days} {weight_span}",
+            view_box: view_box,
             preserve_aspect_ratio: "xMidYMid slice",
             role: "img",
             for entry in entries {
@@ -24,8 +25,8 @@ pub fn Chart() -> Element {
                     circle {
                         cx: (max_date - entry.date).num_days(),
                         cy: weight,
-                        r: 0.25,
-                        fill: "var(--bulma-primary)"
+                        r: if entry.shot.is_some() { 0.4 } else { 0.3 },
+                        fill: get_color(entry.shot)
                     }
                 }
             }
@@ -53,6 +54,29 @@ fn get_weight_range(entries: &[Entry]) -> Result<(f32, f32), ChartError> {
     let min = *weights.first().expect("should be at least 2 weights");
     let max = *weights.last().expect("should be at least 2 weights");
     Ok((min, max))
+}
+
+fn get_color(shot: Option<Shot>) -> String {
+    let color = if let Some(shot) = shot {
+        if shot.dose == 2.5 {
+            "#525252"
+        } else if shot.dose == 5.0 {
+            "#581c87"
+        } else if shot.dose == 7.5 {
+            "#0f766e"
+        } else if shot.dose == 10.0 {
+            "#db2777"
+        } else if shot.dose == 12.5 {
+            "#0ea5e9"
+        } else if shot.dose == 15.0 {
+            "#f87171"
+        } else {
+            "#64748b"
+        }
+    } else {
+        "#374151"
+    };
+    color.to_owned()
 }
 
 #[derive(Debug)]
